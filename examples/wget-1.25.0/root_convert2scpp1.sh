189705e32c87440328d3c8577aad1564268d1603
duneroadrunner <duneroadrunner@gmail.com>
2026-01-20 21:41:43 -0800

    just a little path cleanup

--- examples/wget-1.25.0/root_convert2scpp1.sh
+++ examples/wget-1.25.0/root_convert2scpp1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-././scpptool-master/src/scpptool -ConvertToSCPP -AddPrecedingIncludeConfigDotHDirective -ModifiablePath ./wget-1.25.0/src -ModifiablePath ./wget-build/src/version.c -SuppressPrompts $@ -- -DHAVE_CONFIG_H -DSYSTEM_WGETRC=\"./wget-build/wgetrc\" -DLOCALEDIR=\"./wget-build/share/locale\" -I. -I./wget-1.25.0/src -I./msetl  -I./wget-1.25.0/lib  -I./wget-build/src  -I./wget-build/lib      -DHAVE_LIBSSL -DHAVE_STRLCPY -DO_BINARY=0 -DO_TEXT=0 -DO_SEARCH=O_RDONLY -std=c++23 -c -x c++ > scppt_conv_out1.txt
+./scpptool-master/src/scpptool -ConvertToSCPP -AddPrecedingIncludeConfigDotHDirective -ModifiablePath ./wget-1.25.0/src -ModifiablePath ./wget-build/src/version.c -SuppressPrompts $@ -- -DHAVE_CONFIG_H -DSYSTEM_WGETRC=\"./wget-build/wgetrc\" -DLOCALEDIR=\"./wget-build/share/locale\" -I. -I./wget-1.25.0/src -I./msetl  -I./wget-1.25.0/lib  -I./wget-build/src  -I./wget-build/lib      -DHAVE_LIBSSL -DHAVE_STRLCPY -DO_BINARY=0 -DO_TEXT=0 -DO_SEARCH=O_RDONLY -std=c++23 -c -x c++ > scppt_conv_out1.txt
 
 clang++ -DHAVE_CONFIG_H -DSYSTEM_WGETRC=\"./wget-build/wgetrc\" -DLOCALEDIR=\"./wget-build/share/locale\" -I. -I./wget-1.25.0/src -I./msetl  -I./wget-1.25.0/lib  -I./wget-build/src  -I./wget-build/lib      -DHAVE_LIBSSL -DHAVE_STRLCPY -DO_BINARY=0 -DO_TEXT=0 -DO_SEARCH=O_RDONLY -std=c++23 -g -c -x c++ $@
 
